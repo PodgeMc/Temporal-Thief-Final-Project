e9944a92441c11129eb090b9e39eee25b9dbf2cc
PodgeMc <158078066+PodgeMc@users.noreply.github.com>
2025-11-10 17:13:16 +0000

    first push final year project demo stuff

--- Temporal Thief/Assets/Scripts/DoorController.cs
+++ Temporal Thief/Assets/Scripts/DoorController.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    public Transform door; // the cube that will move like a door
-    public Vector3 openOffset = new Vector3(0, 2.2f, 0); // how far the door moves when opening
-    public float speed = 4f; // how fast the door moves
-
-    private Vector3 closedPos; // where the door starts
-    private Vector3 openPos;   // where the door goes when open
-    private bool isOpen = false; // is the door open right now?
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        closedPos = door.position; // save starting spot
-        openPos = closedPos + openOffset; // figure out where "open" is
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Vector3 target = isOpen ? openPos : closedPos; // pick open or closed target
-        door.position = Vector3.Lerp(door.position, target, Time.deltaTime * speed); // smoothly move door
+        
     }
-
-    public void SetOpen(bool open) => isOpen = open; // call this to open or close door
 }
