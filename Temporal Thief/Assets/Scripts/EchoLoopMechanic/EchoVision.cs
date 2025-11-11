5c967c2aab9029e30a66059394c1ea355f99dbf1
PodgeMc <158078066+PodgeMc@users.noreply.github.com>
2025-11-11 10:00:33 +0000

    added echoVision script, next must properly connect to the paradox script

--- Temporal Thief/Assets/Scripts/EchoLoopMechanic/EchoVision.cs
+++ Temporal Thief/Assets/Scripts/EchoLoopMechanic/EchoVision.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EchoVision : MonoBehaviour
 {
+    //how far can it see
+    public float viewDistance = 5f;
+    //how wide it can see
+    public float viewAngle = 45f;
+    //what layers it can see
+    public LayerMask playerMask;
+    public LayerMask objectMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +24,32 @@ public class EchoVision : MonoBehaviour
         DetectPlayer();
     }
 
-    //how far can it see
-    //how wide it can see
-    //what layers it can see
-
     void DetectPlayer()
     {
         //find anything on the player layer
-        //checik if player is inside cone
-        //no objects in the way of player
+        Collider[] playersInRange = Physics.OverlapSphere(transform.position, viewDistance, playerMask);
+        foreach (Collider player in playersInRange)
+        {
+            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+            //checik if player is inside cone
+            float angle = Vector3.Angle(transform.forward, directionToPlayer);
+            if (angle < viewAngle)
+            {
+                //no objects in the way of player
+                if (!Physics.Raycast(transform.position, directionToPlayer, viewDistance, objectMask));
+                {
+                    ParadoxManager.Instance.TriggerParadox(player.transform.position);
+                }
+            }
+            
+        }
+
+        
     }
     //Draw gizmo to visualize the cone
     private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
     }
 }
